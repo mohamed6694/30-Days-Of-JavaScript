ea3536e0661fbbc4a3bbeecbf2f16de74d08f323
MOHAMMED BOUAALAM <67805200+mohamed6694@users.noreply.github.com>
2025-09-20 11:43:07 +0100

    Update variable.js

--- 01_Day_Introduction/01_day_starter/variable.js
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -15,6 +15,6 @@ const PI = 3.14              // geometrical constant
 
 // Variables can also be declaring in one line separated by comma
 
-let name = 'Asabeneh',     //name of a person
-  job = 'teacher',
-  live = 'Finland'
+let name = 'Mohammed',     //name of a person
+  job = 'Programmer',
+  live = 'Morocco'
